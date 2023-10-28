f764181639c0b6079841e0a750b17bd89e2ef99b
Olivia D. S <hello@palingenae.be>
2023-10-28 20:17:29 +0200

    UPDATE : project creation

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+    return (
+        <footer className="footer">
+            <div className="division">
+                <span className="copyright">&copy; Palingenae { new Date().getFullYear() } - All right reserved.</span>
+            </div>
+            <div className="division -column">
+                <a href="https://palingenae.be" className="link -ui">Main website</a>
+            </div>
+        </footer>
+    )
+}
---
+++ src/components/Navigation.tsx
@@ -0,0 +1,18 @@
+import Logo from '@components/Logo';
+
+export default function Navigation() {
+    return (
+        <nav className="navigation">
+            <div className="navigation__bar">
+                <div className="navigation__logo">
+                <a href="/">
+                    <Logo />
+                </a>
+            </div>
+            <div className="navigation__horizontal">
+                    <a className= "link -ui" href="https://palingenae.be" target='_blank'>Main website</a>
+            </div>
+        </div>
+    </nav>
+    )
+}
